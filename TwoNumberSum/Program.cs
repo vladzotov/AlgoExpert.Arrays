afa3baef168b5738cc23d913807c94c4dcc92955
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-15 13:44:31 +0100

    add TwoNumberSum realization

--- TwoNumberSum/Program.cs
+++ TwoNumberSum/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] inputs = [3, 5, -4, 8, 11, 1, -1, 6];
+int targetSum = 10;
+var result = TwoNumberSum(inputs, targetSum);
 
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+
+static int[] TwoNumberSum(int[] array, int targetSum)
+{
+    Dictionary<int, int> hash = new Dictionary<int, int>();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        var neededItem = targetSum - array[i];
+        if (hash.ContainsKey(neededItem))
+        {
+            return new int[2] { array[i], neededItem };
+        }
+        else
+        {
+            hash.Add(array[i], neededItem);
+        }
+    }
+    return new int[0];
+}
