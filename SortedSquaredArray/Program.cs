164e7ebba26c21dfb40727d49199132109a28ae9
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-15 16:20:22 +0100

    add SortedSquaredArray issue

---
+++ SortedSquaredArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using SortedSquaredArray;
+
+SortedSquaredArrayIssue issue = new SortedSquaredArrayIssue();
+int[] input = [1, 2, 3, 5, 6, 8, 9];
+int[] result = issue.SortedSquaredArray(input);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+namespace SortedSquaredArray
+{
+    public class SortedSquaredArrayIssue
+    {
+        public int[] SortedSquaredArray(int[] array)
+        {
+            int[] result = new int[array.Length];
+            int startIndex = 0;
+            int lastIndex = array.Length - 1;
+
+            for (int i = result.Length - 1; i >= 0; i--)
+            {
+                if (Math.Abs(array[startIndex]) > Math.Abs(array[lastIndex]))
+                {
+                    result[i] = array[startIndex] * array[startIndex];
+                    startIndex++;
+                }
+                else
+                {
+                    result[i] = array[lastIndex] * array[lastIndex];
+                    lastIndex--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/SortedSquaredArrayTests.cs
@@ -0,0 +1,26 @@
+﻿using SortedSquaredArray;
+
+namespace Tests;
+
+[TestClass]
+public class SortedSquaredArrayTests
+{
+    [TestMethod]
+    public void SortedSquaredArray_DefaultCase_SequenceArray()
+    {
+        // Arrange
+        int[] input = [1, 2, 3, 5, 6, 8, 9];
+        int[] expected = [1, 4, 9, 25, 36, 64, 81];
+        SortedSquaredArrayIssue issue = new SortedSquaredArrayIssue();
+
+        // Act
+        int[] result = issue.SortedSquaredArray(input);
+
+        // Assert
+        Assert.IsTrue(result.Length == expected.Length);
+        for (int i = 0; i < result.Length; i++)
+        {
+            Assert.IsTrue(result[i] == expected[i]);
+        }
+    }
+}
