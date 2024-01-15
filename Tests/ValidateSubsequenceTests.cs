e926a96dadb7dcd5e526278a79545020f4cb6662
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-15 15:50:27 +0100

    add ValidateSubsequence issue

---
+++ Tests/ValidateSubsequenceTests.cs
@@ -0,0 +1,35 @@
+﻿using ValidateSubsequence;
+
+namespace Tests;
+
+[TestClass]
+public class ValidateSubsequenceTests
+{
+    [TestMethod]
+    public void IsValidSubsequence_DefaultCase_True()
+    {
+        // Arrange
+        List<int> array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
+        List<int> sequence = new List<int> { 1, 6, -1, 10 };
+
+        // Act
+        var result = ValidateSubsequenceIssue.IsValidSubsequence(array, sequence);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsValidSubsequence_DefaultCase_False()
+    {
+        // Arrange
+        List<int> array = new List<int> { 5, 1, 22, 25, -1, 8, 10 };
+        List<int> sequence = new List<int> { 1, 6, -1, 10 };
+
+        // Act
+        var result = ValidateSubsequenceIssue.IsValidSubsequence(array, sequence);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+}
---
+++ ValidateSubsequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using ValidateSubsequence;
+
+List<int> array = new List<int> { 5, 1, 22, 25, 6, -1, 8, 10 };
+List<int> sequence = new List<int> { 1, 6, -1, 10 };
+
+var result = ValidateSubsequenceIssue.IsValidSubsequence(array, sequence);
+
+Console.WriteLine(result);
+
+namespace ValidateSubsequence
+{
+    public static class ValidateSubsequenceIssue
+    {
+        public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+        {
+            int positionInSequence = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (sequence[positionInSequence] == array[i])
+                {
+                    positionInSequence++;
+                }
+
+                if (positionInSequence == sequence.Count)
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}
