e10dbb4239906b4046eead102a1d4348cadfb07a
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-16 14:10:29 +0100

    add NonConstructibleChange issue

---
+++ NonConstructibleChange/Program.cs
@@ -0,0 +1,38 @@
+﻿using NonConstructibleChange;
+
+int[] coins = [5, 7, 1, 1, 2, 3, 22];
+var result = new NonConstructibleChangeIssue().NonConstructibleChange(coins);
+Console.WriteLine(result);
+
+namespace NonConstructibleChange
+{
+    public class NonConstructibleChangeIssue
+    {
+        /// <summary>
+        /// Each time we can create change + current coin only in case it's not bigger than current change + 1
+        /// [1, 2, 4] -> can create up to 7
+        /// [1, 2, 4, 7] -> can create up to 14 (because 1 + 2 + 4 is 7 and next coin 7 is not bigger than (7 + 1))
+        /// [1, 2, 4, 8] -> can create up to 15 (because 1 + 2 + 4 is 7 and next coin 8 is not bigger than (7 + 1))
+        /// [1, 2, 4, 9] -> can create up to 7 (because 1 + 2 + 4 is 7 and next coin 9 is bigger than (7 + 1))
+        /// </summary>
+        public int NonConstructibleChange(int[] coins)
+        {
+            int change = 0;
+            Array.Sort(coins);
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                if (coins[i] > change + 1)
+                {
+                    return change + 1;
+                }
+                else
+                {
+                    change = change + coins[i];
+                }
+            }
+
+            return change + 1;
+        }
+    }
+}
---
+++ Tests/NonConstructibleChangeTests.cs
@@ -0,0 +1,77 @@
+﻿using NonConstructibleChange;
+
+namespace Tests;
+
+[TestClass]
+public class NonConstructibleChangeTests
+{
+    [TestMethod]
+    public void NonConstructibleChange_DefaultCase_ReturnInteger()
+    {
+        //arrange
+        int[] coins = [5, 7, 1, 1, 2, 3, 22];
+        int expected = 20;
+
+        //act
+        int result = new NonConstructibleChangeIssue().NonConstructibleChange(coins);
+
+        //assert
+        Assert.IsTrue(expected == result);
+    }
+
+    [TestMethod]
+    public void NonConstructibleChange_DefaultCase2_ReturnInteger()
+    {
+        //arrange
+        int[] coins = [1, 2, 4];
+        int expected = 8;
+
+        //act
+        int result = new NonConstructibleChangeIssue().NonConstructibleChange(coins);
+
+        //assert
+        Assert.IsTrue(expected == result);
+    }
+
+    [TestMethod]
+    public void NonConstructibleChange_DefaultCase3_ReturnInteger()
+    {
+        //arrange
+        int[] coins = [1, 2, 4, 7];
+        int expected = 15;
+
+        //act
+        int result = new NonConstructibleChangeIssue().NonConstructibleChange(coins);
+
+        //assert
+        Assert.IsTrue(expected == result);
+    }
+
+    [TestMethod]
+    public void NonConstructibleChange_DefaultCase4_ReturnInteger()
+    {
+        //arrange
+        int[] coins = [1, 2, 4, 8];
+        int expected = 16;
+
+        //act
+        int result = new NonConstructibleChangeIssue().NonConstructibleChange(coins);
+
+        //assert
+        Assert.IsTrue(expected == result);
+    }
+
+    [TestMethod]
+    public void NonConstructibleChange_DefaultCase5_ReturnInteger()
+    {
+        //arrange
+        int[] coins = [1, 2, 4, 9];
+        int expected = 8;
+
+        //act
+        int result = new NonConstructibleChangeIssue().NonConstructibleChange(coins);
+
+        //assert
+        Assert.IsTrue(expected == result);
+    }
+}
