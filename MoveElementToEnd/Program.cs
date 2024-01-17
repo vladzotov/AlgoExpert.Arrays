bef1dde9de69b2c618ea4fc402db05da137653f7
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-17 11:41:40 +0100

    add MoveElementToEnd issue

---
+++ MoveElementToEnd/Program.cs
@@ -0,0 +1,40 @@
+﻿using MoveElementToEnd;
+
+List<int> array = new List<int> { 2, 1, 2, 2, 2, 3, 4, 2 };
+int toMove = 2;
+new MoveElementToEndIssue().MoveElementToEnd(array, toMove);
+
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
+
+namespace MoveElementToEnd
+{
+    public class MoveElementToEndIssue
+    {
+        public List<int> MoveElementToEnd(List<int> array, int toMove)
+        {
+            int startIndex = 0;
+            int lastIndex = array.Count - 1;
+
+            while(startIndex < lastIndex)
+            {
+                if (array[startIndex] == toMove)
+                {
+                    while(array[lastIndex] == toMove && lastIndex > startIndex)
+                    {
+                        lastIndex--;
+                    }
+
+                    int temp = array[lastIndex];
+                    array[lastIndex] = array[startIndex];
+                    array[startIndex] =  temp;
+                }
+                startIndex++;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Tests/MoveElementToEnd.cs
@@ -0,0 +1,26 @@
+﻿using MoveElementToEnd;
+
+namespace Tests;
+
+[TestClass]
+public class MoveElementToEnd
+{
+    [TestMethod]
+    public void MoveElementToEnd_DefaultCase_ChangedArray()
+    {
+        //arrange
+        List<int> array = new List<int> { 2, 1, 2, 2, 2, 3, 4, 2 };
+        int toMove = 2;
+        List<int> expectedStart = new List<int> {4, 1, 3};
+        List<int> expectedEnd = new List<int> {2, 2, 2, 2, 2};
+
+        //act
+        var output = new MoveElementToEndIssue().MoveElementToEnd(array, toMove);
+        List<int> outputStart = output.GetRange(0, 3);
+        List<int> outputEnd = output.GetRange(3, output.Count - 3);
+
+        //assert
+        Assert.IsTrue(outputStart.SequenceEqual(expectedStart));
+        Assert.IsTrue(outputEnd.SequenceEqual(expectedEnd));
+    }
+}
