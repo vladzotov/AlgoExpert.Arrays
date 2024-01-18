25a613b4a707b32b92a3910a70323930e35f11b3
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-18 13:24:31 +0100

    add SmallestDifference issue

---
+++ SmallestDifference/Program.cs
@@ -0,0 +1,58 @@
+﻿using SmallestDifference;
+
+int[] arrayOne = [-1, 5, 10, 20, 28, 3];
+int[] arrayTwo = [26, 134, 135, 15, 17];
+
+int[] result = new SmallestDifferenceIssue().SmallestDifference(arrayOne, arrayTwo);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+namespace SmallestDifference
+{
+    public class SmallestDifferenceIssue
+    {
+        public int[] SmallestDifference(int[] arrayOne, int[] arrayTwo)
+        {
+            Array.Sort(arrayOne);
+            Array.Sort(arrayTwo);
+
+            int[] result = new int[2];
+            int minDifference = int.MaxValue;
+
+            int pointerOne = 0;
+            int pointerTwo = 0;
+
+            while (pointerOne < arrayOne.Length && pointerTwo < arrayTwo.Length)
+            {
+                int numberOne = arrayOne[pointerOne];
+                int numberTwo = arrayTwo[pointerTwo];
+
+                if (numberOne < numberTwo)
+                {
+                    pointerOne++;
+                }
+                else if (numberOne > numberTwo)
+                {
+                    pointerTwo++;
+                }
+                else
+                {
+                    return new int[2] { numberOne, numberTwo };
+                }
+
+                int difference = Math.Abs(numberOne - numberTwo);
+                if (difference < minDifference)
+                {
+                    minDifference = difference;
+                    result[0] = numberOne;
+                    result[1] = numberTwo;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/SmallestDifferenceTests.cs
@@ -0,0 +1,49 @@
+﻿using SmallestDifference;
+
+namespace Tests;
+
+[TestClass]
+public class SmallestDifferenceTests
+{
+    [TestMethod]
+    public void SmallestDifference_DefaultCase_Array()
+    {
+        int[] arrayOne = [-1, 5, 10, 20, 28, 3];
+        int[] arrayTwo = [26, 134, 135, 15, 17];
+        int[] expected = [28, 26];
+
+        int[] result = new SmallestDifferenceIssue().SmallestDifference(arrayOne, arrayTwo);
+
+        Assert.IsTrue(result.Length == 2);
+        Assert.IsTrue(result[0] == expected[0]);
+        Assert.IsTrue(result[1] == expected[1]);
+    }
+
+    [TestMethod]
+    public void SmallestDifference_LastFirst_Array()
+    {
+        int[] arrayOne = [-10, 0, 20, 25];
+        int[] arrayTwo = [1005, 1006, 1014, 1032, 1031];
+        int[] expected = [25, 1005];
+
+        int[] result = new SmallestDifferenceIssue().SmallestDifference(arrayOne, arrayTwo);
+
+        Assert.IsTrue(result.Length == 2);
+        Assert.IsTrue(result[0] == expected[0]);
+        Assert.IsTrue(result[1] == expected[1]);
+    }
+
+    [TestMethod]
+    public void SmallestDifference_FirstLast_Array()
+    {
+        int[] arrayOne = [2, 15, 20, 25];
+        int[] arrayTwo = [-1005, -1006, -1014, -1032, -1031, 7];
+        int[] expected = [2, 7];
+
+        int[] result = new SmallestDifferenceIssue().SmallestDifference(arrayOne, arrayTwo);
+
+        Assert.IsTrue(result.Length == 2);
+        Assert.IsTrue(result[0] == expected[0]);
+        Assert.IsTrue(result[1] == expected[1]);
+    }
+}
