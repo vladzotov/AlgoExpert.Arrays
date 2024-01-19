3afb3f057d302f688b34bc5c176045267a86db78
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-19 11:50:37 +0100

    add first duplicate issue

---
+++ FirstDuplicateValue/Program.cs
@@ -0,0 +1,30 @@
+﻿using FirstDuplicateValue;
+
+int[] array = { 2, 1, 5, 2, 3, 3, 4 };
+int result = new FirstDuplicateValueIssue().FirstDuplicateValue(array);
+Console.WriteLine(result);
+
+namespace FirstDuplicateValue
+{
+    public class FirstDuplicateValueIssue
+    {
+        public int FirstDuplicateValue(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int indexToModify = Math.Abs(array[i]) - 1;
+
+                if (array[indexToModify] > 0)
+                {
+                    array[indexToModify] = array[indexToModify] * -1;
+                }
+                else
+                {
+                    return indexToModify + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
