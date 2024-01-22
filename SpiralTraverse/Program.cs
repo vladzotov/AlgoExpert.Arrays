4a3a3316d508f406afe3e3e1ad271d0e59350f69
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-22 15:43:07 +0100

    add SpiralTraverse issue

---
+++ SpiralTraverse/Program.cs
@@ -0,0 +1,113 @@
+﻿using SpiralTraverse;
+
+// int[,] array = new int[4, 4]
+// {
+//     {1, 2, 3, 4},
+//     {12, 13, 14, 5},
+//     {11, 16, 15, 6},
+//     {10, 9, 8, 7}
+// };
+
+// int[,] array = new int[2, 10]
+// {
+//     {4, 2, 3, 6, 7, 8, 1, 9, 5, 10},
+//     {12, 19, 15, 16, 20, 18, 13, 17, 11, 14}
+// };
+
+// int[,] array = new int[3, 3]
+// {
+//     {1, 2, 3},
+//     {8, 9, 4},
+//     {7, 6, 5}
+// };
+
+// int[,] array = new int[5, 3]
+// {
+//     {1, 2, 3},
+//     {12, 13, 4},
+//     {11, 14, 5},
+//     {10, 15, 6},
+//     {9, 8, 7}
+// };
+
+int[,] array = new int[1, 7]
+{
+    {1, 3, 2, 5, 4, 7, 6}
+};
+
+List<int> resultList = new SpiralTraverseIssue().SpiralTraverse(array);
+foreach (var item in resultList)
+{
+    Console.WriteLine(item);
+}
+
+
+
+namespace SpiralTraverse
+{
+    public class SpiralTraverseIssue
+    {
+        public List<int> SpiralTraverse(int[,] array)
+        {
+            List<int> result = new List<int>(array.Length);
+
+            int startRow = 0;
+            int startColumn = 0;
+            int endRow = array.GetLength(0) - 1;
+            int endColumn = array.GetLength(1) - 1;
+
+            int currentRow = 0;
+            int currentColumn = 0;
+            while (startRow <= endRow && startColumn <= endColumn)
+            {
+                result.Add(array[currentRow, currentColumn]);
+
+                if (currentRow == startRow && currentColumn < endColumn)
+                {
+                    currentColumn++;
+                }
+                else
+
+                if (currentColumn == endColumn && currentRow < endRow)
+                {
+                    currentRow++;
+                }
+                else
+
+                if (currentRow == endRow && currentColumn > startColumn)
+                {
+                    currentColumn--;
+
+                    if (startRow == endRow || startColumn == endColumn)
+                    {
+                        break;
+                    }
+                }
+                else
+
+                if (currentColumn == startColumn && currentRow > startRow)
+                {
+                    currentRow--;
+
+                    if (startRow == endRow || startColumn == endColumn)
+                    {
+                        break;
+                    }
+                }
+
+                if (currentRow == startRow && currentColumn == startColumn)
+                {
+                    startRow++;
+                    startColumn++;
+                    endRow--;
+                    endColumn--;
+
+                    currentRow = startRow;
+                    currentColumn = startColumn;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/SpiralTraverseTests.cs
@@ -0,0 +1,143 @@
+﻿using SpiralTraverse;
+
+namespace Tests;
+
+[TestClass]
+public class SpiralTraverseTests
+{
+    [TestMethod]
+    public void SpiralTraverse_Case1_Array()
+    {
+        //arrange
+        int[,] array = new int[4, 4]
+        {
+            {1, 2, 3, 4},
+            {12, 13, 14, 5},
+            {11, 16, 15, 6},
+            {10, 9, 8, 7}
+        };
+        List<int> expected = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+        //act
+        List<int> result = new SpiralTraverseIssue().SpiralTraverse(array);
+
+        //assert
+        Assert.IsTrue(expected.Count == result.Count);
+        for (int i = 0; i < expected.Count; i++)
+        {
+            Assert.IsTrue(expected[i] == result[i]);
+        }
+    }
+
+    [TestMethod]
+    public void SpiralTraverse_Case2_Array()
+    {
+        //arrange
+        int[,] array = new int[2, 10]
+        {
+            {4, 2, 3, 6, 7, 8, 1, 9, 5, 10},
+            {12, 19, 15, 16, 20, 18, 13, 17, 11, 14}
+        };
+        List<int> expected = new List<int> { 4, 2, 3, 6, 7, 8, 1, 9, 5, 10, 14, 11, 17, 13, 18, 20, 16, 15, 19, 12 };
+
+        //act
+        List<int> result = new SpiralTraverseIssue().SpiralTraverse(array);
+
+        //assert
+        Assert.IsTrue(expected.Count == result.Count);
+        for (int i = 0; i < expected.Count; i++)
+        {
+            Assert.IsTrue(expected[i] == result[i]);
+        }
+    }
+
+    [TestMethod]
+    public void SpiralTraverse_Case3_Array()
+    {
+        //arrange
+        int[,] array = new int[3, 3]
+        {
+            {1, 2, 3},
+            {8, 9, 4},
+            {7, 6, 5}
+        };
+        List<int> expected = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        //act
+        List<int> result = new SpiralTraverseIssue().SpiralTraverse(array);
+
+        //assert
+        Assert.IsTrue(expected.Count == result.Count);
+        for (int i = 0; i < expected.Count; i++)
+        {
+            Assert.IsTrue(expected[i] == result[i]);
+        }
+    }
+
+    [TestMethod]
+    public void SpiralTraverse_Case4_Array()
+    {
+        //arrange
+        int[,] array = new int[5, 3]
+        {
+            {1, 2, 3},
+            {12, 13, 4},
+            {11, 14, 5},
+            {10, 15, 6},
+            {9, 8, 7}
+        };
+        List<int> expected = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+        //act
+        List<int> result = new SpiralTraverseIssue().SpiralTraverse(array);
+
+        //assert
+        Assert.IsTrue(expected.Count == result.Count);
+        for (int i = 0; i < expected.Count; i++)
+        {
+            Assert.IsTrue(expected[i] == result[i]);
+        }
+    }
+
+    [TestMethod]
+    public void SpiralTraverse_Case5_Array()
+    {
+        //arrange
+        int[,] array = new int[1, 7]
+        {
+            {1, 3, 2, 5, 4, 7, 6}
+        };
+        List<int> expected = new List<int> { 1, 3, 2, 5, 4, 7, 6 };
+
+        //act
+        List<int> result = new SpiralTraverseIssue().SpiralTraverse(array);
+
+        //assert
+        Assert.IsTrue(expected.Count == result.Count);
+        for (int i = 0; i < expected.Count; i++)
+        {
+            Assert.IsTrue(expected[i] == result[i]);
+        }
+    }
+
+    [TestMethod]
+    public void SpiralTraverse_Case6_Array()
+    {
+        //arrange
+        int[,] array = new int[1, 1]
+        {
+            {1}
+        };
+        List<int> expected = new List<int> { 1 };
+
+        //act
+        List<int> result = new SpiralTraverseIssue().SpiralTraverse(array);
+
+        //assert
+        Assert.IsTrue(expected.Count == result.Count);
+        for (int i = 0; i < expected.Count; i++)
+        {
+            Assert.IsTrue(expected[i] == result[i]);
+        }
+    }
+}
