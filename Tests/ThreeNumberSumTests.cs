30cad3b4b667b56050989175b0c9e275f34b9d61
Vladyslav Zotov <vladislavzotov94@gmail.com>
2024-01-23 19:13:58 +0100

    add ThreeNumberSum issue

---
+++ Tests/ThreeNumberSumTests.cs
@@ -0,0 +1,72 @@
+﻿using ThreeNumberSum;
+
+namespace Tests;
+
+[TestClass]
+public class ThreeNumberSumTests
+{
+    [TestMethod]
+    public void ThreeNumberSum_Case1_Array()
+    {
+        int[] array = { 12, 3, 1, 2, -6, 5, -8, 6 };
+        int targetSum = 0;
+        List<int[]> expectedTriplets = new List<int[]>()
+        {
+            new int[] { -8, 2, 6 },
+            new int[] { -8, 3, 5 },
+            new int[] { -6, 1, 5 }
+        };
+
+        List<int[]> result = new ThreeNumberSumIssue().ThreeNumberSum(array, targetSum);
+
+        Assert.IsTrue(result.Count == expectedTriplets.Count);
+        for (int i = 0; i < expectedTriplets.Count; i++)
+        {
+            for (int j = 0; j < expectedTriplets[i].Length; j++)
+            {
+                Assert.IsTrue(expectedTriplets[i][j] == result[i][j]);
+            }
+        }
+    }
+
+    [TestMethod]
+    public void ThreeNumberSum_Case2_EmptyArray()
+    {
+        int[] array = { 1, 2, 3 };
+        int targetSum = 7;
+        List<int[]> expectedTriplets = new List<int[]>();
+
+        List<int[]> result = new ThreeNumberSumIssue().ThreeNumberSum(array, targetSum);
+
+        Assert.IsTrue(result.Count == expectedTriplets.Count);
+        for (int i = 0; i < expectedTriplets.Count; i++)
+        {
+            for (int j = 0; j < expectedTriplets[i].Length; j++)
+            {
+                Assert.IsTrue(expectedTriplets[i][j] == result[i][j]);
+            }
+        }
+    }
+
+    [TestMethod]
+    public void ThreeNumberSum_Case3_Array()
+    {
+        int[] array = { 1, 2, 3 };
+        int targetSum = 6;
+        List<int[]> expectedTriplets = new List<int[]>()
+        {
+            new int[] { 1, 2, 3 }
+        };
+
+        List<int[]> result = new ThreeNumberSumIssue().ThreeNumberSum(array, targetSum);
+
+        Assert.IsTrue(result.Count == expectedTriplets.Count);
+        for (int i = 0; i < expectedTriplets.Count; i++)
+        {
+            for (int j = 0; j < expectedTriplets[i].Length; j++)
+            {
+                Assert.IsTrue(expectedTriplets[i][j] == result[i][j]);
+            }
+        }
+    }
+}
---
+++ ThreeNumberSum/Program.cs
@@ -0,0 +1,58 @@
+﻿using ThreeNumberSum;
+
+int[] array = { 12, 3, 1, 2, -6, 5, -8, 6 };
+int targetSum = 0;
+
+List<int[]> result = new ThreeNumberSumIssue().ThreeNumberSum(array, targetSum);
+foreach (var item in result)
+{
+    foreach (var number in item)
+    {
+        Console.Write(number + " ");
+    }
+    Console.WriteLine();
+}
+
+namespace ThreeNumberSum
+{
+    public class ThreeNumberSumIssue
+    {
+        public List<int[]> ThreeNumberSum(int[] array, int targetSum)
+        {
+            List<int[]> result = new List<int[]>();
+
+            Array.Sort(array);
+
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                int leftPointer = i + 1;
+                int rightPointer = array.Length - 1;
+
+                while (leftPointer < rightPointer)
+                {
+                    int currentItem = array[i];
+                    int leftItem = array[leftPointer];
+                    int rightItem = array[rightPointer];
+
+                    int currentSum = currentItem + leftItem + rightItem;
+                    if (currentSum == targetSum)
+                    {
+                        result.Add(new int[] { currentItem, leftItem, rightItem });
+                        leftPointer++;
+                        rightPointer--;
+                    }
+                    else if (currentSum < targetSum)
+                    {
+                        leftPointer++;
+                    }
+                    else if (currentSum > targetSum)
+                    {
+                        rightPointer--;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
